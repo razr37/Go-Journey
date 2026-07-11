70de9fc77dd104b811b67771803ceb4fefc71eeb
razr73 <razr73@gmail.com>
2026-07-11 23:06:19 +0800

    Go Journey v1.1 - Mobile UX improvements

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'go-journey-v1';
+const CACHE = 'go-journey-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
